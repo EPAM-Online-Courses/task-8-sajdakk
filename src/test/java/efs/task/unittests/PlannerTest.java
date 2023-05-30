096d6ff088fce7782eac53340aa06710f68448ef
Anita Sajdak <anita.sajdak123@gmail.com>
2023-05-30 14:59:10 +0200

    add before

--- src/test/java/efs/task/unittests/PlannerTest.java
+++ src/test/java/efs/task/unittests/PlannerTest.java
@@ -2,20 +2,27 @@ package efs.task.unittests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
 public class PlannerTest {
+    private Planner planner;
+
+    @BeforeEach
+    public void init() {
+        this.planner = new Planner();
+    }
+
     @ParameterizedTest(name = "activityLevel={0}")
     @EnumSource(ActivityLevel.class)
     public void shouldReturnCorrectCaloriesDemand_ForAnyActivityLevel(ActivityLevel activityLevel) {
         // given
-        final Planner planner = new Planner();
         final User user = TestConstants.TEST_USER;
 
         // when
-        int result = planner.calculateDailyCaloriesDemand(user, activityLevel);
+        int result = this.planner.calculateDailyCaloriesDemand(user, activityLevel);
 
         // then
         assertEquals(TestConstants.CALORIES_ON_ACTIVITY_LEVEL.get(activityLevel), result);
@@ -24,11 +31,10 @@ public class PlannerTest {
     @Test()
     public void shouldReturnProperDailyIntake_ForGivenUser() {
         // given
-        final Planner planner = new Planner();
         final User user = TestConstants.TEST_USER;
 
         // when
-        DailyIntake result = planner.calculateDailyIntake(user);
+        DailyIntake result = this.planner.calculateDailyIntake(user);
 
         // then
         final DailyIntake expected = TestConstants.TEST_USER_DAILY_INTAKE;
